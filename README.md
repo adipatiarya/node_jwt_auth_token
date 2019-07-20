11c8a74bc174bec2b8bc1274d92d4f95a7acb5bc
Adipati Arya <aryaadipati2@gmail.com>
2019-07-20 08:41:57 +0700

    Add cors

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 NODE JWT AUTH
+
+brew services start mongodb-community@4.0
+
+npm run dev
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const cors = require('cors');
 
 // DB setup
 const mongoose = require('mongoose');
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/auth');
 
 // App setup
 app.use(morgan('combined', true));
+app.use(cors());
 app.use(bodyParser.json());
 router(app);
 
